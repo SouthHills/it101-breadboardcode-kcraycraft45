a214f148e72d1c9c48e894647f569fe79a35f91b
kcraycraft45 <kcraycraft@southhills.edu>
2024-05-01 14:47:39 -0400

    Lab8

--- 08.2_Softlight/Softlight.py
+++ 08.2_Softlight/Softlight.py
@@ -11,6 +11,7 @@ from ADCDevice import *
 USING_GRAVITECH_ADC = False # Only modify this if you are using a Gravitech ADC
 
 LED = PWMLED(17) #17 is the pin
+LED_2 = PWMLED(27)
 ADC = ADCDevice() # Define an ADCDevice class object
 
 def setup():
@@ -33,15 +34,21 @@ def loop():
         # read the ADC value of channel 0
         value = ADC.analogRead(0)  # Gets a value between 0 and 255
         # Set LED brightness directly with value from ADC
+        value_2 = ADC.analogRead(1)
+        
         LED.value = value / 255.0  # Value of PWM LED must be between 0 and 1
+        LED_2.value = value_2 / 255.0
         # calculate the voltage value
         voltage = value / 255.0 * 3.3  # 3.3 because we are using the 3.3V lead
-        print(f'ADC Value: {value} \tVoltage: {voltage:.2f}')
+        voltage_2 = value_2 / 255.0 * 3.3
+        print(f'ADC 0 Value: {value} \tVoltage: {voltage:.2f}')
+        print(f'ADC 1 Value: {value_2} \tVoltage: {voltage_2:.2f}')
         time.sleep(0.03)
 
 def destroy():
     global LED, ADC
     LED.close()
+    LED_2.close()
     ADC.close()
     
 if __name__ == '__main__':   # Program entrance
